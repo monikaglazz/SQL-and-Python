3ad9f88766f2a42c5741335c0ac916caa0e18a1a
Monika <mosiex1998@gmail.com>
2022-08-18 14:25:23 +0200

    Rename 4.sql to DS - 4.sql

--- 4.sql
+++
@@ -1,4 +0,0 @@
-USE shop;
-
-SELECT orders.id, parcels.shipmentDate FROM orders INNER JOIN parcels ON orders.id = parcels.orderID;
-SELECT parcels.id, orders.val, orders.howManyProducts FROM parcels LEFT JOIN orders ON orders.id = parcels.orderID;
